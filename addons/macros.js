f07cb39d6c542539169b89b4a35d827195606f05
Deepak Tanwar <dktanwar@hotmail.com>
2023-02-03 14:15:00 +0000

    shortRNA presentation at the University of Erlangen

---
+++ addons/macros.js
@@ -0,0 +1,4 @@
+remark.macros['abs'] = function(width="30%", left="85%", top="15%", cl="") {
+var url = this;
+return '<img src="' + url + '" style="position:absolute;left:' + left + ';top:' + top + ';width:' + width + '" class="' + cl + '" />';
+};
